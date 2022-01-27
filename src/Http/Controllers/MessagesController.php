b9f707b1738020a377c97c3214703f8d356fd4c2
Lyndon <mcjohnsonlyndon@gmail.com>
2022-01-27 22:55:33 +0000

    avatar attempt and some hidden stuff

--- src/Http/Controllers/MessagesController.php
+++ src/Http/Controllers/MessagesController.php
@@ -82,7 +82,7 @@ class MessagesController extends Controller
         if ($request['type'] == 'user') {
             $fetch = User::where('id', $request['id'])->first();
             if($fetch){
-                $userAvatar = asset('/storage/' . config('chatify.user_avatar.folder') . '/' . $fetch->avatar);
+                $userAvatar = $fetch->profile_photo_url;
             }
         }
 
--- src/views/pages/app.blade.php
+++ src/views/pages/app.blade.php
@@ -46,7 +46,7 @@
            <div class="@if($type == 'group') show @endif messenger-tab groups-tab app-scroll" data-view="groups">
                 {{-- items --}}
                 <p style="text-align: center;color:grey;margin-top:30px">
-                    <a target="_blank" style="color:{{$messengerColor}};" href="https://chatify.munafio.com/notes#groups-feature">Click here</a> for more info!
+                    This feature will be released soon.
                 </p>
              </div>
 
