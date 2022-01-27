d0bd08c4274eb176a97f4528926d4cb608f7ebd2
Lyndon <mcjohnsonlyndon@gmail.com>
2022-01-27 23:27:58 +0000

    user names update

--- src/views/layouts/listItem.blade.php
+++ src/views/layouts/listItem.blade.php
@@ -26,14 +26,12 @@
             @if($user->active_status)
                 <span class="activeStatus"></span>
             @endif
-        <div class="avatar av-m"
-        style="background-image: url('{{ $user->profile_photo_url }}');">
-        </div>
+        <div class="avatar av-m" style="background-image: url('{{ $user->profile_photo_url }}');"></div>
         </td>
         {{-- center side --}}
         <td>
         <p data-id="{{ $user->id }}" data-type="user">
-            {{ strlen($user->name) > 12 ? trim(substr($user->name,0,12)).'..' : $user->name }}
+            {{ strlen($user->name()) > 12 ? trim(substr($user->name(),0,12)).'..' : $user->name() }}
             <span>{{ $lastMessage->created_at->diffForHumans() }}</span></p>
         <span>
             {{-- Last Message user indicator --}}
@@ -74,7 +72,7 @@
         {{-- center side --}}
         <td>
             <p data-id="{{ $user->id }}" data-type="user">
-            {{ strlen($user->name) > 12 ? trim(substr($user->name,0,12)).'..' : $user->name }}
+            {{ strlen($user->name()) > 12 ? trim(substr($user->name(),0,12)).'..' : $user->name() }}
         </td>
 
     </tr>
